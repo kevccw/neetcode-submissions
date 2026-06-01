668f1f3fdc4fc89fecc550ffeef287b4f17a1426
Kevin Chen <39719941+kevccw@users.noreply.github.com>
2026-06-01 16:37:29 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "hello"
+print(type(variable))
+variable = ['world']
+print(type(variable))
